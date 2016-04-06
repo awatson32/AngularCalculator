68076673df266aebeaddf910038f220d5e28fc58
awatson32 <audrey.watson32@gmail.com>
2016-04-06 10:52:53 -0500

    Attempting to fix the number checking for blank input boxes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,57 @@
  */
 angular.module('angularCalculatorApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    
+    $scope.Add = function(op1, op2) {
+          op1 = $scope.op1;
+          op2 = $scope.op2;
+        if (op1 || op2 === '&nbsp;') {
+            console.log('Enter a number');
+        } else {
+            var result = op1 + op2;
+            console.log(result); 
+        }
+            
+        // var result = op1 + op2;
+        
+        // if(op1 || op2 === ' ') {
+        //     console.log('You must enter something!');
+        // } else {
+            
+            
+        // }
+        
+    };
+    
+    $scope.Subtract = function(op1, op2) {
+        op1 = $scope.op1;
+        op2 = $scope.op2;
+        
+        var result = op1 - op2;
+        console.log(result);
+    };
+    
+    $scope.Multiply = function() {
+        var op1 = $scope.op1;
+        var op2 = $scope.op2;
+        
+        var result = op1 * op2;
+        console.log(result);
+    };
+    
+    $scope.Divide = function () {
+        var op1 = $scope.op1;
+        var op2 = $scope.op2;
+        
+        if (op2 === 0) {
+            console.log('You cannot divide by zero. Think of the world.');
+        } else {
+            var result = op1 / op2;
+            console.log(result);
+        }
+        
+    };
+    
+    
   });
