e00c1f909fc7486f9c37da86054d19534fecf430
awatson32 <audrey.watson32@gmail.com>
2016-04-06 11:21:48 -0500

    All is working except for checking for blank fields.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,58 +8,54 @@
  * Controller of the angularCalculatorApp
  */
 angular.module('angularCalculatorApp')
-  .controller('MainCtrl', function ($scope) {
-    
-    
-    $scope.Add = function(op1, op2) {
-          op1 = $scope.op1;
-          op2 = $scope.op2;
-        if (op1 || op2 === '&nbsp;') {
-            console.log('Enter a number');
-        } else {
-            var result = op1 + op2;
-            console.log(result); 
-        }
+    .controller('MainCtrl', function($scope) {
+        
+        $scope.Add = function() {
             
-        // var result = op1 + op2;
-        
-        // if(op1 || op2 === ' ') {
-        //     console.log('You must enter something!');
-        // } else {
+            var op1 = $scope.op1;
+            var op2 = $scope.op2;
             
+            if (op1 || op2 === null) {
+                alert('Enter a number');
+            } else {
+                var result = op1 + op1;
+                alert('The result of adding ' + op1 + ' and ' + op2 + ' is ' + result + '.');
+            }
             
-        // }
-        
-    };
-    
-    $scope.Subtract = function(op1, op2) {
-        op1 = $scope.op1;
-        op2 = $scope.op2;
-        
-        var result = op1 - op2;
-        console.log(result);
-    };
-    
-    $scope.Multiply = function() {
-        var op1 = $scope.op1;
-        var op2 = $scope.op2;
-        
-        var result = op1 * op2;
-        console.log(result);
-    };
-    
-    $scope.Divide = function () {
-        var op1 = $scope.op1;
-        var op2 = $scope.op2;
-        
-        if (op2 === 0) {
-            console.log('You cannot divide by zero. Think of the world.');
-        } else {
-            var result = op1 / op2;
-            console.log(result);
-        }
-        
-    };
-    
-    
-  });
+            // if (op1 || op2 === '') {
+            //     console.log('Enter a number');
+            // } else {
+            // }
+        };
+
+        $scope.Subtract = function(op1, op2) {
+            op1 = $scope.op1;
+            op2 = $scope.op2;
+
+            var result = op1 - op2;
+            alert('The result of substracting' + op2 + ' from ' + op1 + ' is ' + result + '.');
+        };
+
+        $scope.Multiply = function() {
+            var op1 = $scope.op1;
+            var op2 = $scope.op2;
+
+            var result = op1 * op2;
+            alert('The result of multipyling ' + op1 + ' and ' + op2 + ' is ' + result + '.');
+        };
+
+        $scope.Divide = function() {
+            var op1 = $scope.op1;
+            var op2 = $scope.op2;
+
+            if (op2 === 0) {
+                alert('You cannot divide by zero. Think of the world.');
+            } else {
+                var result = op1 / op2;
+                alert('The result of dividing ' + op1 + ' by ' + op2 + ' is ' + result + '.');
+            }
+
+        };
+
+
+    });
